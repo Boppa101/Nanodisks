0b6f7727c15f614d0aa1c8a9daadbba31b0a3275
Paul Schlaugat <poolberlinbonn@gmail.com>
2025-01-10 11:09:38 +0100

    Starting on eigenvalue problem

--- main.c
+++ main.c
@@ -14,6 +14,56 @@ void PrintArr(int N, double* Arr) {
     printf("\n");
 }
 
+/* Auxiliary routines prototypes */
+extern void print_matrix( char* desc, MKL_INT m, MKL_INT n, MKL_Complex16* a, MKL_INT lda );
+
+/* Parameters */
+#define N 4
+#define LDA N
+#define LDVL N
+#define LDVR N
+
+/* Main program */
+int main() {
+    /* Locals */
+    MKL_INT n = N, lda = LDA, ldvl = LDVL, ldvr = LDVR, info;
+    /* Local arrays */
+    MKL_Complex16 w[N], vl[LDVL*N], vr[LDVR*N];
+    MKL_Complex16 a[LDA*N] = {
+        {-3.84,  2.25}, {-8.94, -4.75}, { 8.95, -6.53}, {-9.87,  4.82},
+        {-0.66,  0.83}, {-4.40, -3.82}, {-3.50, -4.26}, {-3.15,  7.36},
+        {-3.99, -4.73}, {-5.88, -6.60}, {-3.36, -0.40}, {-0.75,  5.23},
+        { 7.74,  4.18}, { 3.66, -7.53}, { 2.58,  3.60}, { 4.59,  5.41}
+    };
+    /* Executable statements */
+    printf( "LAPACKE_zgeev (row-major, high-level) Example Program Results\n" );
+    /* Solve eigenproblem */
+    info = LAPACKE_zgeev( LAPACK_ROW_MAJOR, 'V', 'V', n, a, lda, w, vl, ldvl, vr, ldvr );
+    /* Check for convergence */
+    if( info > 0 ) {
+        printf( "The algorithm failed to compute eigenvalues.\n" );
+        exit( 1 );
+    }
+    /* Print eigenvalues */
+    print_matrix( "Eigenvalues", 1, n, w, 1 );
+    /* Print left eigenvectors */
+    print_matrix( "Left eigenvectors", n, n, vl, ldvl );
+    /* Print right eigenvectors */
+    print_matrix( "Right eigenvectors", n, n, vr, ldvr );
+    exit( 0 );
+} /* End of LAPACKE_zgeev Example */
+
+/* Auxiliary routine: printing a matrix */
+void print_matrix( char* desc, MKL_INT m, MKL_INT n, MKL_Complex16* a, MKL_INT lda ) {
+    MKL_INT i, j;
+    printf( "\n %s\n", desc );
+    for( i = 0; i < m; i++ ) {
+        for( j = 0; j < n; j++ )
+            printf( " (%6.2f,%6.2f)", a[i*lda+j].real, a[i*lda+j].imag );
+        printf( "\n" );
+    }
+}
+
 // FillTheta(N, ThetaArr);
 // PrintArr(N, ThetaArr);
 // double complex eta = 1 + 1*I;
@@ -22,74 +72,125 @@ void PrintArr(int N, double* Arr) {
 //     printf("%f + %f\n", creal(ThetaArr[i]), cimag(ThetaArr[i]));
 // }
 
-int main(void) {
-    int N = 100;
-    int cutoff = 100;
-
-    double* ThetaArr = (double*)malloc(sizeof(double)*N);
-    FillTheta(N, ThetaArr);
-
-    double* M_m0 = (double*)malloc(sizeof(double)*N*N);
-    // double* M_m1 = (double*)malloc(sizeof(double)*N*N);
-
-    FillM(N, 0, ThetaArr, cutoff, M_m0);
-    // FillM(N, 1, ThetaArr, cutoff, M_m1);
-
-    double EF = 0.36/au_eV;
-    double omega = 0.2/au_eV;
-    double gamma = 0.02/au_eV;
-    double a = 25/au_nm;
-
-    double complex eta = I*Drude(EF, omega, gamma)/(omega*a);
-
-    // for(int i=0; i<N*N; i++) {
-    //     M_m0[i] = eta*M_m0[i];
-    //     // M_m1[i] = eta*M_m1[i];
-    // }
-
-    double eigenvalues[N];
-    MKL_INT n = N, lda = N, info;
-    info = LAPACKE_dsyev(LAPACK_COL_MAJOR, 'V', 'U', n, M_m0, lda, eigenvalues);
-
-    if (info > 0) {
-        printf("The algorithm failed to compute eigenvalues.\n");
-        return 1;
-    }
-
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (eigenvalues[j] > eigenvalues[j + 1]) {
-                // Swap eigenvalues
-                double temp_val = eigenvalues[j];
-                eigenvalues[j] = eigenvalues[j + 1];
-                eigenvalues[j + 1] = temp_val;
-
-                // Swap corresponding eigenvectors
-                for (int k = 0; k < n; k++) {
-                    double temp_vec = M_m0[k + j * lda];
-                    M_m0[k + j * lda] = M_m0[k + (j + 1) * lda];
-                    M_m0[k + (j + 1) * lda] = temp_vec;
-                }
-            }
-        }
-    }
-
-    printf("Eigenvalues:\n");
-    for (int i = 0; i < n; i++) {
-        printf("%lf\n", eigenvalues[i]);
-    }
-
-    // printf("Eigenvectors:\n");
-    // for (int i = 0; i < 5; i++) {
-    //     for (int j = 0; j < n; j++) {
-    //         printf("%lf ", creal(M_m0[j + i * lda]));
-    //     }
-    //     printf("\n");
-    // }
-
-    free(ThetaArr);
-    free(M_m0);
-    // free(M_m1);
-
-    return 0;
-}
+// int main(void) {
+//     // Define the matrix (complex values)
+//     // MKL_Complex16 A[4] = {{1.0, 1.0}, {0.0, 2.0}, {-1.0, -1.0}, {0.0, -2.0}};
+//     MKL_Complex16 A[4] = {{1.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {1.0, 0.0}};
+//     MKL_Complex16 W[2]; // Eigenvalues
+//     MKL_Complex16 VL[4]; // Left eigenvectors
+//     MKL_Complex16 VR[4]; // Right eigenvectors
+//
+//     MKL_INT n = 2; // 2x2 matrix
+//     MKL_INT lda = n, ldvl = n, ldvr = n;
+//     MKL_INT lwork = -1; // Query optimal workspace size
+//     MKL_Complex16 work_query[1];
+//     double rwork[2];
+//     MKL_INT info;
+//
+//     // Query the optimal workspace size
+//     zgeev("N", "V", &n, A, &lda, W, VL, &ldvl, VR, &ldvr, work_query, &lwork, rwork, &info);
+//
+//     if (info != 0) {
+//         printf("Error querying workspace size: %lld\n", info);
+//         return -1;
+//     }
+//
+//     // Allocate the optimal workspace
+//     lwork = (int)work_query[0].real; // Optimal size is stored here
+//     MKL_Complex16 work[lwork];
+//
+//     // Compute eigenvalues and eigenvectors
+//     zgeev("N", "V", &n, A, &lda, W, VL, &ldvl, VR, &ldvr, work, &lwork, rwork, &info);
+//
+//     if (info == 0) {
+//         printf("Eigenvalues and eigenvectors computed successfully.\n");
+//     } else {
+//         printf("Error in computation: %lld\n", info);
+//     }
+//
+//     // Output eigenvalues
+//     for (int i = 0; i < n; i++) {
+//         printf("Eigenvalue %d: %.2f + %.2fi\n", i, W[i].real, W[i].imag);
+//     }
+//
+//     // Output right eigenvectors (columns of VR)
+//     printf("\nRight eigenvectors (columns of VR):\n");
+//     for (int i = 0; i < n; i++) {
+//         for (int j = 0; j < n; j++) {
+//             printf("VR[%d][%d]: %.2f + %.2fi  ", j, i, VR[i * n + j].real, VR[i * n + j].imag);
+//         }
+//         printf("\n");
+//     }
+//     // int N = 3;
+//     // int cutoff = 100;
+//     //
+//     // double* ThetaArr = (double*)malloc(sizeof(double)*N);
+//     // FillTheta(N, ThetaArr);
+//     //
+//     // double* M_m0 = (double*)malloc(sizeof(double)*N*N);
+//     // // double* M_m1 = (double*)malloc(sizeof(double)*N*N);
+//     //
+//     // FillM(N, 0, ThetaArr, cutoff, M_m0);
+//     // // FillM(N, 1, ThetaArr, cutoff, M_m1);
+//     //
+//     // double EF = 0.36/au_eV;
+//     // double omega = 0.2/au_eV;
+//     // double gamma = 0.02/au_eV;
+//     // double a = 25/au_nm;
+//     //
+//     // double complex eta = I*Drude(EF, omega, gamma)/(omega*a);
+//     //
+//     // for(int i=0; i<N*N; i++) {
+//     //     M_m0[i] = eta*M_m0[i];
+//     //     // M_m1[i] = eta*M_m1[i];
+//     // }
+//     //
+//     // double eigenvalues[N];
+//     // MKL_INT n = N, lda = N, info;
+//     // info = LAPACKE_dsyev(LAPACK_ROW_MAJOR, 'V', 'U', n, M_m0, lda, eigenvalues);
+//     //
+//     // if (info > 0) {
+//     //     printf("The algorithm failed to compute eigenvalues.\n");
+//     //     return 1;
+//     // }
+//     //
+//     // for (int i = 0; i < n - 1; i++) {
+//     //     for (int j = 0; j < n - i - 1; j++) {
+//     //         if (eigenvalues[j] > eigenvalues[j + 1]) {
+//     //             // Swap eigenvalues
+//     //             double temp_val = eigenvalues[j];
+//     //             eigenvalues[j] = eigenvalues[j + 1];
+//     //             eigenvalues[j + 1] = temp_val;
+//     //
+//     //             // Swap corresponding eigenvectors
+//     //             for (int k = 0; k < n; k++) {
+//     //                 double temp_vec = M_m0[k + j * lda];
+//     //                 M_m0[k + j * lda] = M_m0[k + (j + 1) * lda];
+//     //                 M_m0[k + (j + 1) * lda] = temp_vec;
+//     //             }
+//     //         }
+//     //     }
+//     // }
+//     //
+//     // double complex eigenvalues_c[N];
+//     // for(int i=0; i<N; i++) eigenvalues_c[i] = eta*eigenvalues[i];
+//     //
+//     // printf("Eigenvalues:\n");
+//     // for (int i = 0; i < n; i++) {
+//     //     printf("%lf+%lf\n", creal(eigenvalues_c[i]), cimag(eigenvalues_c[i]));
+//     // }
+//     //
+//     // printf("Eigenvectors (scaled):\n");
+//     // for (int i = 0; i < n; i++) {
+//     //     for (int j = 0; j < n; j++) {
+//     //         printf("%lf ", M_m0[j + i * lda]);
+//     //     }
+//     //     printf("\n");
+//     // }
+//     //
+//     // free(ThetaArr);
+//     // free(M_m0);
+//     // // free(M_m1);
+//
+//     return 0;
+// }
